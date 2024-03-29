289ceb4010c291f74c4320d77465e5aade82df30
carter <speers.carter@gmail.com>
2024-03-29 12:22:05 -0400

    updated to v0.2

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 AllowedVehiclesList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
 
-PROMPT = """
-********************************
-AutoCountry Vehicle Finder v0.1
+PROMPT = """********************************
+AutoCountry Vehicle Finder v0.2
 ********************************
 Please Enter the following number below from the following menu:
 
 1. PRINT all Authorized Vehicles
-2. Exit
+2. SEARCH for Authorized Vehicles
+3. Exit
 """
+VEHICLE_SEARCH_PROMPT = """********************************
+Please enter the full vehicle name:"""
+while (True):
+    decision = input(PROMPT)
 
-decision = input(PROMPT)
+    if (decision == '1'):
+        print('The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:')
+        for vehicle in AllowedVehiclesList:
+            print(vehicle)
+    elif (decision == '2'):
+        vehicle_name = input(VEHICLE_SEARCH_PROMPT)
+        if (vehicle_name in AllowedVehiclesList):
+            print(f'{vehicle_name} is an authorized vehicle')
+        else:
+            print(f'{vehicle_name} is not an authorized vehicle, if you received this in error please check the spelling and try again')
 
-if (decision == '1'):
-    print('The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:')
-    for vehicle in AllowedVehiclesList:
-        print(vehicle)
-elif (decision == '2'):
-    print('Thank you for using the AutoCountry Vehicle Finder, good-bye!')
+    elif (decision == '3'):
+        print('Thank you for using the AutoCountry Vehicle Finder, good-bye!')
+        break;
 
 
