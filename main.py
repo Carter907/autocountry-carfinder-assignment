ee04e3d39cebd7755d1c88694dbecacc11d004a7
Carter S <speers.carter@gmail.com>
2024-04-28 18:58:52 -0400

    0-6 using functions

--- main.py
+++ main.py
@@ -20,43 +20,51 @@ Please Enter the following number below from the following menu:
 VEHICLE_SEARCH_PROMPT = """********************************
 Please enter the full vehicle name:"""
 
-def remove_vehicle(vehicle: str):
-    allowed_vehicles.remove(vehicle)
-    write_vehicles();
+def delete_vehicle(vehicle: str):
+    
+    choice = input(f'Are you sure you want to remove "{vehicle_name}" from the Authorized Vehicles List?\n')
+    if choice == 'yes':
+        allowed_vehicles.remove(vehicle)
+        write_vehicles();
+        print(f'You have REMOVED "{vehicle_name}" as an authorized vehicle')
 
 def add_vehicle(vehicle: str):
     allowed_vehicles.append(vehicle)
     write_vehicles();
+    print(f'You have added "{vehicle_name}" as an authorized vehicle')
 
 def write_vehicles():
     with open('allowed_vehicles.txt', 'w') as vehicles:
         for vehicle in allowed_vehicles:
             vehicles.write(vehicle + '\n')
 
+def print_vehicles():
+    print('The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:')
+    for vehicle in allowed_vehicles:
+        print(vehicle)
+    write_vehicles();
+
+def search_vehicle(vehicle: str):
+    if (vehicle_name in allowed_vehicles):
+        print(f'{vehicle_name} is an authorized vehicle')
+    else:
+        print(f'{vehicle_name} is not an authorized vehicle, if you received this in error please check the spelling and try again')
+
 
 while (True):
     decision = input(PROMPT)
 
     if (decision == '1'):
-        print('The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:')
-        for vehicle in allowed_vehicles:
-            print(vehicle)
+        print_vehicles()
     elif (decision == '2'):
         vehicle_name = input(VEHICLE_SEARCH_PROMPT)
-        if (vehicle_name in allowed_vehicles):
-            print(f'{vehicle_name} is an authorized vehicle')
-        else:
-            print(f'{vehicle_name} is not an authorized vehicle, if you received this in error please check the spelling and try again')
+        search_vehicle(vehicle_name)
     elif (decision == '3'):
         vehicle_name = input(f'Please Enter the full Vehicle name you would like to ADD:\n')
         add_vehicle(vehicle_name);
-        print(f'You have added "{vehicle_name}" as an authorized vehicle')
     elif (decision == '4'):
         vehicle_name = input(f'Please Enter the full Vehicle name you would like to REMOVE:\n')
-        choice = input(f'Are you sure you want to remove "{vehicle_name}" from the Authorized Vehicles List?\n')
-        if choice == 'yes':
-            remove_vehicle(vehicle_name)
-            print(f'You have REMOVED "{vehicle_name}" as an authorized vehicle')
+        delete_vehicle(vehicle_name)
             
     elif (decision == '5'):
         print('Thank you for using the AutoCountry Vehicle Finder, good-bye!')
